2c645be134259da1f220970e41999998398572d0
An <and2900@inbox.ru>
2021-03-16 21:44:37 +0300

    Task 3 draft

--- task_3/boolean_search.py
+++ task_3/boolean_search.py
@@ -1,14 +1,56 @@
 import codecs
 import os
 from argparse import ArgumentParser
-from typing import List
+from typing import List, Set
 
 from task_3.utils import load_dict, load_inverted_index_from_file
 
+
 # TODO: Возможно, добавить обработку NOT
-def find_documents_in_index_by_word(word_id : int, inverted_index : List[List[int]], ):
+def find_documents_in_index_by_word(word_id: int, inverted_index: List[List[int]], num_documents: int,
+                                    get_present: bool = False) -> Set[int]:
+    """
+    :param word_id: Идентификатор слова, для которого находится список документов, в
+    которых оно содержится/не содержится (содержание/не содержание зависит от значения
+    параметра get_present)
+    :param inverted_index: Инвертированный индекс документов: список из <размер словаря>
+    списков, содержащий идентификаторы документов, в которых содержится соответствующее
+    слово
+    :param num_documents: Число документов коллекции. Используется для того, чтобы взять
+    дополнение списка документов до всей коллекции документов для нахождения документов,
+    в которых не содержится указанное слово с идентификатором word_id.
+    :param get_present: True для взятия списка идентификаторов документов, в которых
+    содержится слово с идентификатором word_id, иначе берётся дополнение этого списка до
+    всей коллекции
+    :return: Список уникальных номеров документов без повторений, в которых содержится/
+    не содержится слово с идентификатором word_id
+    """
     documents_list = inverted_index[word_id]
+    if not get_present:
+        all_docs_set = set(range(num_documents))
+        documents_list = all_docs_set.difference(documents_list)
     return documents_list
+
+
+def combine_docs_sets(docs_sets_list: List[Set[int]], operation : str) -> Set[int]:
+    """
+    Получает на вход список наборов идентификаторов документов и применяет к
+    ним либо операцию объединения, либо операцию пересечения.
+    :param docs_sets_list: Список наборов идентификаторов документов,
+    к которым применяется операция типа operation. Набор идентификаторов
+    содержит уникальные идентификаторы без повторений.
+    :param operation: Тип операции: union/intersection, которая применяется
+    к набору идентификаторов документов: объединение/пересечение.
+    :return: Результат выполнения операций на списке наборов идентификаторов
+    документов - один набор идентификаторов документов.
+    """
+    if operation == "union":
+        resulting_set = set.union(docs_sets_list)
+    elif operation == "intersection":
+        resulting_set = set.intersection(docs_sets_list)
+    else:
+        raise ValueError(f"Invalid set operation: {operation}")
+    return resulting_set
 
 
 
@@ -19,6 +61,8 @@ def main():
                         help="")
     parser.add_argument('--input_dict_path', default=r"../task_2/tokenized_texts/dict.txt", type=str,
                         help="")
+    parser.add_argument('--input_reviews_file', default=r"", type=str,
+                        help="")
     parser.add_argument('--output_inv_index_path', default=r"inverted_index/inv_index.txt", type=str,
                         help="Выходной путь файла инвертированного индекса")
 
@@ -28,10 +72,6 @@ def main():
 
     token2id = load_dict(input_dict_path)
     inverted_index = load_inverted_index_from_file(input_inv_index_path)
-
-
-
-
 
 
 if __name__ == '__main__':
